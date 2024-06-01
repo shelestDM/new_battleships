14a247e6967aed0f0218b09c1f4af17f3779c6c8
bartashevich <shelest.dm@icloud.com>
2024-06-01 11:54:37 +0300

    update new

--- app.js
+++ app.js
@@ -22,13 +22,11 @@ let currentDroppable = null;
 battleship_element.onmousedown = function (e) { // добавляем слушатель события на корабль нажатия мышки
     let shiftX = e.clientX - battleship_element.getBoundingClientRect().left; //сдвиг по оси Х
     let shiftY = e.clientY - battleship_element.getBoundingClientRect().top; //сдвиг по оси У
-    console.log(shiftX, shiftY);
     let isInDropZone = false;
 
     moveAt(e.pageX, e.pageY);
     let new_left = 0;
     let new_top = 0;
-    console.log(shiftX);
 
     function moveAt(pageX, pageY) {
         battleship_element.style.left = pageX - shiftX + 'px';
@@ -99,49 +97,68 @@ function dragNDropForShipInField(e, ship_element) {
     let shiftX = e.clientX - ship_element.getBoundingClientRect().left; //сдвиг по оси Х
     let shiftY = e.clientY - ship_element.getBoundingClientRect().top; //сдвиг по оси У
 
+    let aligned_top_coordinate = 0;
+    let aligned_left_coordinate = 0;
+
     moveAt(e.pageX, e.pageY);
     function moveAt(pageX, pageY) {
-
-        let old_coordinate_left = ship_element.getBoundingClientRect().left - player_one_board_element.getBoundingClientRect().left;
-        let new_coordinate_left = Math.floor((pageX - player_one_board_element.getBoundingClientRect().left - shiftX) / cell_size) * cell_size;
-        let max_available_coordinate_to_set_left = (cell_count_x_y - ship_element.getBoundingClientRect().width / cell_size) * cell_size;
-        let left_coordinate_condition = new_coordinate_left > old_coordinate_left;
-
-        if (left_coordinate_condition) {
-            if (new_coordinate_left > max_available_coordinate_to_set_left) {
-                ship_element.style.left = max_available_coordinate_to_set_left + 'px'
-            } else {
-                ship_element.style.left = new_coordinate_left + 'px'
-            }
-        }
-
-        let old_coordinate_top = ship_element.getBoundingClientRect().top - player_one_board_element.getBoundingClientRect().top;
-        let new_coordinate_top = Math.floor((pageY - player_one_board_element.getBoundingClientRect().top - shiftY) / cell_size) * cell_size;
-        let max_available_coordinate_to_set_top = (cell_count_x_y - ship_element.getBoundingClientRect().height / cell_size) * cell_size;
-        let top_coordinate_condition = new_coordinate_top > old_coordinate_top;
-
-
-        if (top_coordinate_condition) {
-            if (new_coordinate_top > max_available_coordinate_to_set_top) {
-                ship_element.style.top = max_available_coordinate_to_set_top + 'px'
-            } else {
-                ship_element.style.top = new_coordinate_top + 'px'
-            }
-        }
-
-
-
+        ship_element.style.left = pageX - player_one_board_element.getBoundingClientRect().left - shiftX + 'px';
+        ship_element.style.top = pageY -  player_one_board_element.getBoundingClientRect().top - shiftY + 'px';
     }
 
     function onMouseMove(e) {// обработчиком события движения мышки
         moveAt(e.pageX, e.pageY);
+        ship_element.hidden = true;
+        let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+        ship_element.hidden = false;
+
+        if (!elemBelow) return;
+
+        let droppableBelow = elemBelow.closest('.droppable');
+
+        if (currentDroppable != droppableBelow) {
+
+            currentDroppable = droppableBelow;
+            console.log('droppableBelow',droppableBelow);
+           
+        }
+        let max_available_coordinate_to_set_left = (cell_count_x_y - ship_element.getBoundingClientRect().width / cell_size) * cell_size;
+        let max_available_coordinate_to_set_top = (cell_count_x_y - ship_element.getBoundingClientRect().height / cell_size) * cell_size;
+        let new_coordinate_left = ship_element.getBoundingClientRect().left - player_one_board_element.getBoundingClientRect().left + shiftX;
+        let new_coordinate_top = ship_element.getBoundingClientRect().top - player_one_board_element.getBoundingClientRect().top + shiftY;
+        
+
+        aligned_left_coordinate = Math.floor(new_coordinate_left/cell_size) * cell_size;
+        aligned_top_coordinate =  Math.floor(Math.abs(new_coordinate_top/cell_size)) * cell_size;
+
+        
+        if(new_coordinate_top<-35){
+            aligned_top_coordinate = 0;
+        }
+
+        if(new_coordinate_left<0){
+            aligned_left_coordinate = 0;
+        }
+
+        if(new_coordinate_left>max_available_coordinate_to_set_left){
+            aligned_left_coordinate = max_available_coordinate_to_set_left;
+        }
+
+        if(new_coordinate_top>max_available_coordinate_to_set_top){
+            aligned_top_coordinate = max_available_coordinate_to_set_top;
+        }
+
     }
 
-    document.addEventListener("mousemove", onMouseMove);
 
+    //привязка на движение мышкой
+    document.addEventListener("mousemove", onMouseMove);
+    //отвязываем обработчик события
     ship_element.onmouseup = function () {
         document.removeEventListener("mousemove", onMouseMove);
         ship_element.onmouseup = null;
+        ship_element.style.left = aligned_left_coordinate + 'px';
+        ship_element.style.top = aligned_top_coordinate + 'px';
     }
 
     ship_element.ondragstart = function () {
